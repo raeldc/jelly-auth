b62bce376680fc22e43e59b9119f79b90f4ddd37
Israel D. Canasa <raeldc@wizmediateam.com>
2010-04-06 22:17:39 +0800

    I thought I cleaned everything already. But thanks  guillermin. Fixes #2

--- classes/model/user/token.php
+++ classes/model/user/token.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die ('No direct script access.');
 /**
- * Sprig Auth User Token
- * @package Sprig Auth
- * @author	Paul Banks
+ * Model User Token
+ * @package Jelly Auth
+ * @author	Israel Canasa
  */
 class Model_User_Token extends Model_Auth_User_Token {}
